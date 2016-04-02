dec94a3aca44d3145955f0203ef8777c7aba005e
Gonzo <gonzalo.mendez@quantumfactory.com.ar>
2016-04-02 13:48:26 -0300

    va

---
+++ Site/Site/Scripts/tuServicioApp.js
@@ -0,0 +1,52 @@
+/// <reference path="../scripts/typings/angularjs/angular.d.ts" />
+/// <reference path="../scripts/typings/angularjs/angular-route.d.ts" />
+var OneStopTechVidsApp;
+(function (OneStopTechVidsApp) {
+    var Config = (function () {
+        function Config($routeProvider) {
+            $routeProvider.when("/list", { templateUrl: "App/Templates/VideoList.html", controller: "TechVidsListCtrl" })
+                .when("/list/:id", { templateUrl: "App/Templates/VideoList.html", controller: "TechVidsListCtrl" })
+                .when("/add", { templateUrl: "App/Templates/AddVideo.html", controller: "AddTechVideoCtrl" })
+                .when("/edit/:id", { templateUrl: "App/Templates/EditVideo.html", controller: "EditTechVideoCtrl" })
+                .otherwise({ redirectTo: '/list' });
+        }
+        return Config;
+    })();
+    OneStopTechVidsApp.Config = Config;
+    Config.$inject = ['$routeProvider'];
+    var TechVidsCategoryCtrl = (function () {
+        function TechVidsCategoryCtrl() {
+        }
+        return TechVidsCategoryCtrl;
+    })();
+    OneStopTechVidsApp.TechVidsCategoryCtrl = TechVidsCategoryCtrl;
+    TechVidsCategoryCtrl.$inject = ['$scope', 'techVidsDataSvc'];
+    var TechVidsListCtrl = (function () {
+        function TechVidsListCtrl() {
+        }
+        return TechVidsListCtrl;
+    })();
+    OneStopTechVidsApp.TechVidsListCtrl = TechVidsListCtrl;
+    TechVidsListCtrl.$inject = ['$scope', '$routeParams', 'techVidsDataSvc'];
+    var EditTechVideoCtrl = (function () {
+        function EditTechVideoCtrl() {
+        }
+        return EditTechVideoCtrl;
+    })();
+    OneStopTechVidsApp.EditTechVideoCtrl = EditTechVideoCtrl;
+    EditTechVideoCtrl.$inject = ['$scope', '$routeParams', '$window', 'techVidsDataSvc'];
+    var AddVideoCtrl = (function () {
+        function AddVideoCtrl() {
+        }
+        return AddVideoCtrl;
+    })();
+    OneStopTechVidsApp.AddVideoCtrl = AddVideoCtrl;
+    AddVideoCtrl.$inject = ['$scope', '$window', 'techVidsDataSvc'];
+    var UniqueVideoTitle = (function () {
+        function UniqueVideoTitle() {
+        }
+        return UniqueVideoTitle;
+    })();
+    OneStopTechVidsApp.UniqueVideoTitle = UniqueVideoTitle;
+    var app = angular.module("techVidsApp", ['ngRoute']);
+})(OneStopTechVidsApp || (OneStopTechVidsApp = {}));
--- Site/Site/Scripts/tuServicioApp.ts
+++ Site/Site/Scripts/tuServicioApp.ts
@@ -1,9 +1,47 @@
-﻿module tuServicioApp {
-    //var app = angular.module("tuServicioApp", []);
-    /*app.filter('greet', function () {
-        return function (name) {
-            alert('hola desde typescript con angular!');
-        };
-    });*/
+﻿/// <reference path="../scripts/typings/angularjs/angular.d.ts" />
+/// <reference path="../scripts/typings/angularjs/angular-route.d.ts" />
 
+module Extensions {
+
+}
+
+module OneStopTechVidsApp {
+    export class Config {
+        constructor($routeProvider: ng.route.IRouteProvider) {
+            $routeProvider.when("/list", { templateUrl: "App/Templates/VideoList.html", controller: "TechVidsListCtrl" })
+                .when("/list/:id", { templateUrl: "App/Templates/VideoList.html", controller: "TechVidsListCtrl" })
+                .when("/add", { templateUrl: "App/Templates/AddVideo.html", controller: "AddTechVideoCtrl" })
+                .when("/edit/:id", { templateUrl: "App/Templates/EditVideo.html", controller: "EditTechVideoCtrl" })
+                .otherwise({ redirectTo: '/list' });
+        }
+    }
+    Config.$inject = ['$routeProvider'];
+
+    export class TechVidsCategoryCtrl {
+
+    }
+    TechVidsCategoryCtrl.$inject = ['$scope', 'techVidsDataSvc'];
+
+    export class TechVidsListCtrl {
+
+
+
+    }
+
+    TechVidsListCtrl.$inject = ['$scope', '$routeParams', 'techVidsDataSvc'];
+
+    export class EditTechVideoCtrl {
+
+    }
+    EditTechVideoCtrl.$inject = ['$scope', '$routeParams', '$window', 'techVidsDataSvc'];
+
+    export class AddVideoCtrl {
+    }
+
+    AddVideoCtrl.$inject = ['$scope', '$window', 'techVidsDataSvc'];
+
+    export class UniqueVideoTitle {
+    }
+
+    var app = angular.module("techVidsApp", ['ngRoute']);
 }
