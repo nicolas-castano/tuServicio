dac211bc0c9894036cfe81373ff8b609f7b19b83
Gonzo <gonzalo.mendez@quantumfactory.com.ar>
2016-04-16 21:09:44 -0300

    intento de que funcione login

--- Site/Site/Scripts/tuServicioApp.js
+++ Site/Site/Scripts/tuServicioApp.js
@@ -1,53 +1,17 @@
 /// <reference path="../scripts/typings/angularjs/angular.d.ts" />
 /// <reference path="../scripts/typings/angularjs/angular-route.d.ts" />
-var OneStopTechVidsApp;
-(function (OneStopTechVidsApp) {
-    var Config = (function () {
-        function Config($routeProvider) {
-            $routeProvider.when("/list", { templateUrl: "App/Templates/VideoList.html", controller: "TechVidsListCtrl" })
-                .when("/list/:id", { templateUrl: "App/Templates/VideoList.html", controller: "TechVidsListCtrl" })
-                .when("/add", { templateUrl: "App/Templates/AddVideo.html", controller: "AddTechVideoCtrl" })
-                .when("/edit/:id", { templateUrl: "App/Templates/EditVideo.html", controller: "EditTechVideoCtrl" })
-                .otherwise({ redirectTo: '/list' });
+var tuServicio;
+(function (tuServicio) {
+    var loginController = (function () {
+        function loginController() {
         }
-        return Config;
-    })();
-    OneStopTechVidsApp.Config = Config;
-    Config.$inject = ['$routeProvider'];
-    var TechVidsCategoryCtrl = (function () {
-        function TechVidsCategoryCtrl() {
-        }
-        return TechVidsCategoryCtrl;
-    })();
-    OneStopTechVidsApp.TechVidsCategoryCtrl = TechVidsCategoryCtrl;
-    TechVidsCategoryCtrl.$inject = ['$scope', 'techVidsDataSvc'];
-    var TechVidsListCtrl = (function () {
-        function TechVidsListCtrl() {
-        }
-        return TechVidsListCtrl;
-    })();
-    OneStopTechVidsApp.TechVidsListCtrl = TechVidsListCtrl;
-    TechVidsListCtrl.$inject = ['$scope', '$routeParams', 'techVidsDataSvc'];
-    var EditTechVideoCtrl = (function () {
-        function EditTechVideoCtrl() {
-        }
-        return EditTechVideoCtrl;
-    })();
-    OneStopTechVidsApp.EditTechVideoCtrl = EditTechVideoCtrl;
-    EditTechVideoCtrl.$inject = ['$scope', '$routeParams', '$window', 'techVidsDataSvc'];
-    var AddVideoCtrl = (function () {
-        function AddVideoCtrl() {
-        }
-        return AddVideoCtrl;
-    })();
-    OneStopTechVidsApp.AddVideoCtrl = AddVideoCtrl;
-    AddVideoCtrl.$inject = ['$scope', '$window', 'techVidsDataSvc'];
-    var UniqueVideoTitle = (function () {
-        function UniqueVideoTitle() {
-        }
-        return UniqueVideoTitle;
-    })();
-    OneStopTechVidsApp.UniqueVideoTitle = UniqueVideoTitle;
+        loginController.prototype.login = function () {
+            alert('aa');
+            //console.log('Login was clicked, username is ${this.name} and password is ${this.password}');
+        };
+        return loginController;
+    }());
+    tuServicio.loginController = loginController;
     var app = angular.module("tuServicioApp", ['ngRoute']);
-})(OneStopTechVidsApp || (OneStopTechVidsApp = {}));
-//# sourceMappingURL=tuServicioApp.js.map
+    app.controller('loginController', tuServicio.loginController);
+})(tuServicio || (tuServicio = {}));
--- Site/Site/Scripts/tuServicioApp.ts
+++ Site/Site/Scripts/tuServicioApp.ts
@@ -5,43 +5,18 @@ module Extensions {
 
 }
 
-module OneStopTechVidsApp {
-    export class Config {
-        constructor($routeProvider: ng.route.IRouteProvider) {
-            $routeProvider.when("/list", { templateUrl: "App/Templates/VideoList.html", controller: "TechVidsListCtrl" })
-                .when("/list/:id", { templateUrl: "App/Templates/VideoList.html", controller: "TechVidsListCtrl" })
-                .when("/add", { templateUrl: "App/Templates/AddVideo.html", controller: "AddTechVideoCtrl" })
-                .when("/edit/:id", { templateUrl: "App/Templates/EditVideo.html", controller: "EditTechVideoCtrl" })
-                .otherwise({ redirectTo: '/list' });
+module tuServicio {
+    export class loginController {
+        name: string;
+        password: string;
+        constructor() { }
+
+        login() {
+            alert('aa');
+            //console.log('Login was clicked, username is ${this.name} and password is ${this.password}');
         }
-    }
-    Config.$inject = ['$routeProvider'];
-
-    export class TechVidsCategoryCtrl {
-
-    }
-    TechVidsCategoryCtrl.$inject = ['$scope', 'techVidsDataSvc'];
-
-    export class TechVidsListCtrl {
-
-
-
-    }
-
-    TechVidsListCtrl.$inject = ['$scope', '$routeParams', 'techVidsDataSvc'];
-
-    export class EditTechVideoCtrl {
-
-    }
-    EditTechVideoCtrl.$inject = ['$scope', '$routeParams', '$window', 'techVidsDataSvc'];
-
-    export class AddVideoCtrl {
-    }
-
-    AddVideoCtrl.$inject = ['$scope', '$window', 'techVidsDataSvc'];
-
-    export class UniqueVideoTitle {
     }
 
     var app = angular.module("tuServicioApp", ['ngRoute']);
+    app.controller('loginController', tuServicio.loginController);
 }
